4d04eade1e0aa6f8d1fca08e1d38ae3b0c214928
Matthew Andrews <matthew@andrews.eu.com>
2013-05-25 02:17:13 +0100

    Add deets to error

--- test/build.js
+++ test/build.js
@@ -4,4 +4,7 @@ var proxyquire = require('proxyquireify');
 proxyquire.browserify()
   .require(require.resolve('./test'), { entry: true })
   .bundle()
-  .pipe(fs.createWriteStream(__dirname + '/build/fruitmachine-domdelegate.test.js'));
+  .pipe(fs.createWriteStream(__dirname + '/build/fruitmachine-domdelegate.test.js'))
+  .on('error', function(deets) {
+    console.log(deets);
+  });
